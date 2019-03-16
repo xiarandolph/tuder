ba4f07acf695acdf02557440a56bb3e2d6c0f599
bendgk <benkosten@gmail.com>
2019-03-16 14:26:48 -0400

    basic login form

--- client/src/main.js
+++ client/src/main.js
@@ -2,10 +2,29 @@ import Vue from 'vue'
 import './plugins/vuetify'
 import App from './App.vue'
 import router from './router'
+import store from './store'
+
+import $ from "jquery"
+window.jQuery = $
 
 Vue.config.productionTip = false
 
+//handle local authentication of routes
+router.beforeEach((to, from, next) => {
+    const publicPages = ['/auth'];
+    const authRequired = !publicPages.includes(to.path);
+    //const loggedIn = JSON.parse(localStorage.getItem('user'));
+    const loggedIn = false;
+
+    if (authRequired && !loggedIn) {
+        return next('/auth');
+    }
+
+    next();
+})
+
 new Vue({
   router,
+  store,
   render: h => h(App)
 }).$mount('#app')
---
+++ client/src/views/Auth.vue
@@ -0,0 +1,43 @@
+<template>
+    <v-app id="inspire">
+        <v-content>
+            <v-container fluid fill-height>
+                <v-layout align-center justify-center>
+                    <v-flex xs12 sm8 md4>
+                        <v-card class="elevation-12">
+                            <v-toolbar dark color="primary">
+                                <v-toolbar-title>Login form</v-toolbar-title>
+                                <v-spacer></v-spacer>
+                                <v-btn depressed="true" color="white" block="true">Register</v-btn>
+                                <v-btn depressed="true" color="white" block="true">Login</v-btn>
+                            </v-toolbar>
+                            <v-card-text>
+                                <v-form>
+                                    <v-text-field prepend-icon="person" name="email" label="Email" type="text" placeholder="student@rpi.edu"></v-text-field>
+                                   
+                                    <v-text-field id="password" prepend-icon="lock" name="password" label="Password" type="password"></v-text-field>
+                                </v-form>
+                            </v-card-text>
+                            <v-card-actions>
+                                <v-spacer></v-spacer>
+                                <v-btn color="primary">Login</v-btn>
+                            </v-card-actions>
+                        </v-card>
+                    </v-flex>
+                </v-layout>
+            </v-container>
+        </v-content>
+    </v-app>
+</template>
+
+<script>
+    export default {
+      data: () => ({
+        drawer: null
+      }),
+    
+      props: {
+        source: String
+      }
+    }
+</script>
